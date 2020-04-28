ca68bf6a6d39cb950e3d1e2f70a0f0479b04c639
Maisoun Rashed <mrashed2@ugrad3.cs.jhu.edu>
2020-04-28 03:04:26 -0400

    blocked out the functions for basci game rules

--- basicgamerules.cpp
+++ basicgamerules.cpp
@@ -3,6 +3,7 @@
 //bascigamerules.cpp
 
 #include "basicgamerules.h"
+#include "maze.h"
 
 BasicGameRules::BasicGameRules(const BasicGameRules &){
 }
@@ -10,19 +11,50 @@ BasicGameRules::BasicGameRules(const BasicGameRules &){
 BasicGameRules::~BasicGameRules() {
 }
 
-<<<<<<< HEAD
-bool BasicGameRule::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
-  
-=======
-bool BasicGameRules::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
 
->>>>>>> 0621c6c8c2d1ad6c0dfe74c01be65b6469fc07cf
+bool BasicGameRules::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
+  bool move = false;
+  //check if destination tile is unoccupied and if the tile's checkMoveOnto function allows the move
+
+  //checks if dest is unoccupied:
+  /*if(tile.getGlyph() != "#" &&  ) {
+    move = true;
+    }*/
+
+  //checking that the source is only one away from the destination
+  if(source.distanceFrom(dest) == 1) {
+    move = true;
+  }
+
+  //CASE2
+  //if theres something that has property v(moveable) and that the tile adjacent to the tile in the same direction
+  /*else if( ) {
+    
+    }*/
+  
+  return move;
 }
 
 void BasicGameRules::enactMove(Game *game, Entity *actor, const Position &dest) const {
 
+  //carries out move if the move's approved
+  if(BasicGameRules::allowmove) {
+    actor.setPosition(dest);
+  }
+  //if theres an inanimate object
 }
 
 GameResult BasicGameRules::checkGameResult(Game *game) const {
-
+  GameResult result;
+  Tile::Tile = Maze::getTile(dest) 
+    //if(actor.hasProperty('h')){
+    //  result = HERO_WINS;
+    //}
+    //else if(entity.hasProperty('m')'s position is the same as the entity w prop 'h') {
+    //  result = HERO_LOSES;
+    //}
+    //else {
+    //  result = UNKNOWN;
+    //}
+  return result;
 }
--- inanimate.cpp
+++ inanimate.cpp
@@ -6,10 +6,13 @@
 #include "inanimate.h"
 Inanimate::Inanimate() {
 }
+
 Inanimate::~Inanimate() {
 }
 
 Direction Inanimate::getMoveDirection(Game *game, Entity *entity) {
 }
+
+
 bool Inanimate::isUser() const {
 }
