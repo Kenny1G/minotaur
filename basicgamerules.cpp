2229afc8e33201d0f119fc3e63ab2be4ca668c90
Maisoun Rashed <mrashed2@ugrad3.cs.jhu.edu>
2020-04-29 03:28:20 -0400

    basicgamerules hopefully done

--- basicgamerules.cpp
+++ basicgamerules.cpp
@@ -4,6 +4,11 @@
 
 #include "basicgamerules.h"
 #include "maze.h"
+#include "game.h"
+#include "tile.h"
+#include "position.h"
+#include "entity.h"
+
 
 BasicGameRules::BasicGameRules(const BasicGameRules &){
 }
@@ -15,46 +20,70 @@ BasicGameRules::~BasicGameRules() {
 bool BasicGameRules::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
   bool move = false;
   //check if destination tile is unoccupied and if the tile's checkMoveOnto function allows the move
-
-  //checks if dest is unoccupied:
-  /*if(tile.getGlyph() != "#" &&  ) {
-    move = true;
-    }*/
-
-  //checking that the source is only one away from the destination
   if(source.distanceFrom(dest) == 1) {
-    move = true;
+    //checks if dest is unoccupied:
+    if(tile.getGlyph() != "#" && MoveResult::checkMoveOnto == ALLOW) {
+      move = true;
+    }
+    
+    
+    //CASE2
+    //if theres something that has property v(moveable) and that the tile adjacent to the tile in the same direction
+    Entity::Entity *en = Game::getEntityAt(dest);
+    else if(!en == NULL ) {
+      if(en.hasProperty('v') {
+	  Position::Position new_dest = dest;
+	  new_dest.displace(actor.TextUI::getMoveDirection());
+	  return allowMove(game, actor, dest, new_dest);
+	}
+	else if(en.hasProperty('m') {
+	    move = true;
+	  }
+	  }
+	}
+      return move;
   }
 
-  //CASE2
-  //if theres something that has property v(moveable) and that the tile adjacent to the tile in the same direction
-  /*else if( ) {
-    
-    }*/
-  
-  return move;
-}
-
 void BasicGameRules::enactMove(Game *game, Entity *actor, const Position &dest) const {
+  //if theres an inanimate object                                                                                                                             
+  Entity::Entity *en = Game::getEntityAt(dest);
+  if(!en == NULL && en.hasProperty('v')) {
+    Position::Position new_dest	= dest;
+    new_dest.displace(actor.TEXTUI::getMoveDirection());
+    if(Game::getEntityAt(new_dest) != NULL) {
+      BasicGameRules::enactMove(game, actor, new_dest); 
+    }
+    else {
+      en.setPosition(new_dest);
+    }
+  }
 
   //carries out move if the move's approved
   if(BasicGameRules::allowMove()) {
     actor.setPosition(dest);
   }
-  //if theres an inanimate object
+
 }
 
 GameResult BasicGameRules::checkGameResult(Game *game) const {
-  GameResult result;
-  Tile::Tile = Maze::getTile(dest) 
-    //if(actor.hasProperty('h')){
-    //  result = HERO_WINS;
-    //}
-    //else if(entity.hasProperty('m')'s position is the same as the entity w prop 'h') {
-    //  result = HERO_LOSES;
-    //}
-    //else {
-    //  result = UNKNOWN;
-    //}
+  GameResult result = UNKNOWN;
+  Game::EntityVec h_vec = Game::getEntitiesWithProperty('h');
+  Game::EntityVec m_vec = Game::getEntitiesWithProperty('m');
+  
+  Entity::Entity h = h_vec[0];
+  Entity::Entity m = m_vec[0];
+
+  Position::Postion hp = h.Entity::getPosition();
+  Position::Postion mp = m.Entity::getPosition();
+
+  
+  
+  if(hp.isGoal()){
+    result = HERO_WINS;
+  }
+  else if(hp == mp) {
+      result = HERO_LOSES;
+  }
   return result;
+  
 }
