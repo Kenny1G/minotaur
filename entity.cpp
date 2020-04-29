0496d96adc577eda1d83abb12b921c1a07745122
Victoria Subritzky Katz <vsubrit1@jh.edu>
2020-04-29 18:15:19 -0400

    Implimentation of entity.cpp and header

--- entity.cpp
+++ entity.cpp
@@ -14,45 +14,52 @@ Entity::~Entity() {}
 // Set the glyph that will represent this Entity.
 // Should consist of a single character.
 void Entity::setGlyph(const std::string &glyph) {
+  m_glyph = glyph;
 }
 
 // Set the properties of this Entity.  Each character in the
 // string represents a single property.
 void Entity::setProperties(const std::string &props) {
+  m_properties = props;
 }
 
 // Get the glyph representing this Entity.
 std::string Entity::getGlyph() const {
-	return "M";
+	return m_glyph;
 }
 
 // Get the string containing this Entity's properties.
 std::string Entity::getProperties() const {
-	return "JOJO";
+	return m_properties;
 }
 
 // Return true if the Entity's properties contain the specified
 // character, false otherwise.
 bool Entity::hasProperty(char prop) const {
-	return true;
+  bool result = false; 
+  if (m_properties == prop){
+    result = true;
+  }
+  return result;
 }
 
 // Set this Entity's EntityController, which will determine how it moves.
 // The Entity assumes responsibility for deleting it.
 void Entity::setController(EntityController *controller) {
+  m_controller = controller;
 }
 
 // Get this Entity's EntityController.
 EntityController Entity::*getController() {
-	return nullptr;
+  return m_controller;
 }
 
 // Set this Entity's Position.
 void Entity::setPosition(const Position &pos) {
+  m_pos = pos;
 }
 
 // Return this Entity's Position.
 Position Entity::getPosition() const {
-	Position *pos = new Position();
-	return *pos;
+  return m_pos;
 }
