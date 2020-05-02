7ab621939d421785df488b27c645674941f01391
Maisoun Rashed <mrashed2@ugrad3.cs.jhu.edu>
2020-05-02 18:32:58 -0400

    astar header file

---
+++ astar.h
@@ -0,0 +1,16 @@
+// Kenny O., Victoria S.K., Miso R., kosele01, vsubrit1, mrashed2                                   
+// CS220 Final Project SP20
+// astar.h
+
+#ifndef ASTAR_H
+#define ASTAR_H
+
+#include 
+
+class AStar {
+private:
+  Maze m_maze;
+  
+
+#endif //ASTAR_H
+}
